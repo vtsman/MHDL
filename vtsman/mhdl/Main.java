8322c764efff27097608df5503e9fedf0d6e7112
vtsman <hokiespencer@gmail.com>
2013-11-19 21:46:06 -0500

    Second Commit
    Not much here...

--- vtsman/mhdl/Main.java
+++ vtsman/mhdl/Main.java
@@ -3,13 +3,13 @@ package vtsman.mhdl;
 import java.util.HashMap;
 
 public class Main {
-	static String lines[] = {"WIRE w", "OR (w FALSE)", "OR (out w FALSE)"};//, "PART test {", "}"};
+	static String lines[] = {"OR (w FALSE)", "AND (q w FALSE)", "WIRE w", "WIRE q", "XOR (out w q)"};
 	public static void main(String[] args) {
 		Wire w = new Wire();
 		Wire[] ws = {new Wire()};
 		Program p = Compiler.compile(lines, w, ws);
 		p.exec();
-		System.out.println(p.out.get());
+		System.out.println(w.get());
 	}
 
 }
